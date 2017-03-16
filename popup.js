5dbc389025bcdfb1292dcecfe2a1ef67f556851c
Pieter Kockx <pieterkockx@gmail.com>
2017-03-16 15:50:35 +0100

    Save matching bookmarks in list

--- popup.js
+++ popup.js
@@ -1,8 +1,12 @@
 window.addEventListener('DOMContentLoaded', function() {
-  var button = document.getElementById('button');
   var box = document.getElementById('box');
+  var button = document.getElementById('button');
+  var results;
+  results = crawlBookmarks(box.value);
   button.addEventListener('click', function() {
-    search(box.value)
+    results.forEach(function(obj) {
+      createTab(obj.url);
+    });
   });
 });
 
@@ -14,17 +18,23 @@ function createTab(url) {
   chrome.tabs.create(createProperties);
 }
 
-function search(term) {
+function crawlBookmarks(term) {
+  var results = [];
   var match = new RegExp('%s');
   function traverse(trees) {
     trees.forEach(function(tree) {
       if (tree.hasOwnProperty('children') && tree.children.length > 0)
         traverse(tree.children);
-      else if (tree.hasOwnProperty('url') && match.test(tree.url))
-        createTab(tree.url.replace('%s', encodeURIComponent(term)));
+      else if (tree.hasOwnProperty('url') && match.test(tree.url)) {
+        var result = Object.create(null);
+        result['url'] = tree.url.replace('%s', encodeURIComponent(term));
+        result['title'] = tree.title;
+        results.push(result);
+      }
     });
   }
   chrome.bookmarks.getTree(function(trees) {
     traverse(trees);
   });
+  return results;
 }
